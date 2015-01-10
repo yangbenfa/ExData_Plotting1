a5e69fedd7e59c5ab71d8101349f61654eee7836
yangbenfa <276296011@qq.com>
2015-01-10 14:59:04 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 
+
 #读取数据（2007年2月1日与2007年2月2日的数据）
 data<-read.csv2("household_power_consumption.txt",stringsAsFactors=FALSE,skip=66636,nrows=2880)
 
@@ -7,11 +8,11 @@ names(data)<-c("Date","Time","Global_active_power","Global_reactive_power",
                "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 #将Date和Time变量转换为R中的Date/Time类
 
-#合并Date和Time为一个字段DT
-data$DT<- paste(data$Date, data$Time)
+#合并Date和Time为一个字段datetime
+data$datetime<- paste(data$Date, data$Time)
 
 #完成转换（转换为R中的Date/Time类）
-data$DT<- strptime(data$DT, "%d/%m/%Y %H:%M:%S")
+data$datetime<- strptime(data$datetime, "%d/%m/%Y %H:%M:%S")
 #转换数据类型
 for(i in 3:9)
 {
@@ -29,4 +30,4 @@ dev.copy(png, file = "plot1.png")
 dev.off() 
 
 
-
+#第2个代码及图形
